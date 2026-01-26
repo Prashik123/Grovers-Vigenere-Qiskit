b6e4871190ff57af973d80be3094da7ddabd2e94
Prashik Somkuwar <55804903+Prashik123@users.noreply.github.com>
2026-01-26 17:36:51 +0530

    Create frequency.py

---
+++ classical/frequency.py
@@ -0,0 +1,16 @@
+import numpy as np
+import string
+
+ENGLISH_FREQ = np.array([
+    8.167,1.492,2.782,4.253,12.702,2.228,2.015,
+    6.094,6.966,0.153,0.772,4.025,2.406,6.749,
+    7.507,1.929,0.095,5.987,6.327,9.056,2.758,
+    0.978,2.360,0.150,1.974,0.074
+])
+
+def score_plaintext(text):
+    counts = np.zeros(26)
+    for c in text:
+        counts[ord(c) - 65] += 1
+    freq = counts / len(text) * 100
+    return np.sum((freq - ENGLISH_FREQ)**2 / ENGLISH_FREQ)
